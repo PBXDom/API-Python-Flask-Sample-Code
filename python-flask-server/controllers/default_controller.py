f2776840d1f8d534f43b98c39e0c929156cc7f10
PBXDom <PBXDom@users.noreply.github.com>
2016-07-04 09:15:35 -0700

    Add files via upload

---
+++ python-flask-server/controllers/default_controller.py
@@ -0,0 +1,12 @@
+
+def calls_get(rptType, rptId, start = None, limit = None, sortBy = None, sortType = None, fromDate = None, toDate = None, duration = None, phone = None, phone1 = None, co = None, ext = None, pbxId = None, callSource = None, callType = None, direction = None, callerName = None, did = None, dnis = None, acc = None, ring = None, cost = None, group = None) -> str:
+    return 'do some magic!'
+
+def features_charts_get() -> str:
+    return 'do some magic!'
+
+def features_reports_get() -> str:
+    return 'do some magic!'
+
+def features_widget_get() -> str:
+    return 'do some magic!'
